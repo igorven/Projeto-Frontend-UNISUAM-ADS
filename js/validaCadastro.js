b079b9c56826897c6b187335aa483b88916e3866
igorven <souzaigorfabricio11@gmail.com>
2026-06-10 20:36:27 -0300

    Cadastro retrabalhado
    O sistema de cadastros foi retrabalhado, permitindo validação dos dados, busca de CEP via API (ViaCEP) e o cálculo do CPF, além de permitir verificar se a senha atende as condições e também são iguais
    Junto com esses recursos, o localStorage foi adicionado, permitindo o armazenamento dos dados inseridos nos campos

--- js/validaCadastro.js
+++ js/validaCadastro.js
@@ -1,26 +1,58 @@
+const form = document.getElementById("formCadastro");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
 /*== Elementos do formulário de cadastro ==*/
-const nome = document.getElementById("name");
+const nome = document.getElementById("nome");
 const cpf = document.getElementById("cpf");
 const cep = document.getElementById("cep");
+const rua = document.getElementById("rua");
+const bairro = document.getElementById("bairro");
+const cidade = document.getElementById("cidade");
+const estado = document.getElementById("estado");
+const telefone = document.getElementById("telefone");
 const email = document.getElementById("email");
-const senha = document.getElementById("password");
-const confirmarSenha = document.getElementById("confirmpassword");
+const senha = document.getElementById("senha");
+const confirmarSenha = document.getElementById("confirmarSenha");
 
-/**== Funções de validação ==*/
+const mensagem = document.getElementById("mensagemCadastro");
+
+/*== Mensagem ==*/
+function mostrarMensagem(texto, cor = "green") {
+
+    mensagem.textContent = texto;
+    mensagem.style.color = cor;
+
+}
+
+/*== Máscara do CPF ==*/
+cpf.addEventListener("input", () => {
+
+    let valor = cpf.value.replace(/\D/g, "");
+
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+
+    cpf.value = valor;
+
+});
 
 /**== CPF ==*/
 
+
 cpf.addEventListener("blur", () => {
 
-    const cpfmsg = document.getElementById("cpfmsg");
-
+    const erroCpf = document.getElementById("erroCpf")
     if(validarCPF(cpf.value)) {
-        cpfmsg.textContent = "CPF válido.";
-        cpfmsg.className = "ok";
+        erroCpf.textContent = "CPF válido.";
+        cpf.classList.add("input-ok");
     }
 
     else {
-        cpfmsg.textContent = ""
+        erroCpf.textContent = "CPF inválido.";
     }
 })
 
@@ -32,53 +64,65 @@ function validarCPF(cpf) {
     let soma = 0;
     let resto;
 
-    for (let i = 1; i <=9; i++)
-        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    for (let i = 0; i < 9; i++)
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
 
     resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(cpf.substring(9, 10))) return false;
+    if (resto === 10 || resto === 11)
+        { resto = 0 };
+    if (resto !== parseInt(cpf.charAt(9))) return false;
 
     soma = 0;
-    for (let i = 1; i <= 10; i++)
-        soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+    for (let i = 0; i < 10; i++)
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
 
     resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0;
+    if (resto === 10 || resto === 11)
+        { resto = 0 };
 
-    return resto === parseInt(cpf.substring(10, 11));
+    return resto === parseInt(cpf.charAt(10));
 }
 
 /**== CEP (utilizando o API ViaCEP) ==*/
-document.getElementById("cep").addEventListener("blur", function() {
-    const cep = this.value.replace(/\D/g, '');
-    const cepErro = document.getElementById("cepErro")
+cep.addEventListener("input", findCEP)
 
 
-    cepErro.textContent = "";
+function findCEP() {
+    let cepValor = cep.value.replace(/\D/g, "");
 
-    if (cep.length !== 8) {
-     cepErro.textContent = "CEP inválido."   
-     return; 
-    }
+    if (cepValor.length !== 8) return;
 
-    fetch(`https://viacep.com.br/ws/${cep}/json`)
+    fetch(`https://viacep.com.br/ws/${cepValor}/json/`)
         .then(res => res.json())
         .then(data => {
+
             if (data.erro) {
-                cepErro.textContent = "CEP não encontrado.";
+                mostrarMensagem("CEP não encontrado", "red");
                 return;
             }
-            
-            document.getElementById("rua").value = data.logradouro;
-            document.getElementById("bairro").value = data.bairro;
-            document.getElementById("cidade").value = data.localidade;
+
+            rua.value = data.logradouro || "";
+            bairro.value = data.bairro || "";
+            cidade.value = data.localidade || "";
+            estado.value = data.uf || "";
+
         })
-        
         .catch(() => {
-            cepErro.textContent = "Erro ao buscar CEP";
+            mostrarMensagem("Erro ao buscar CEP", "red");
         });
-})
+};
+
+/**== Telefone ==*/
+telefone.addEventListener("input", () => {
+
+    let valor = telefone.value.replace(/\D/g, "");
+
+    valor = valor.replace(/^(\d{2})(\d)/, "($1) $2");
+    valor = valor.replace(/(\d{5})(\d)/, "$1-$2");
+
+    telefone.value = valor;
+
+});
 
 /**== E-mail ==*/
 
@@ -90,42 +134,107 @@ function validarEmail(email) {
 
 
 email.addEventListener("input", () => {
-    const msg = document.getElementById("emailmsg");
+    const msg = document.getElementById("mensagemCadastro");
 
     if (validarEmail(email.value)) {
         msg.textContent = "E-mail válido.";
-        msg.className = "success";
         email.classList.add("input-ok");
     }
 
     else {
         msg.textContent = "E-mail inválido.";
-        msg.className = "erro";
+        msg.classList.remove("input-ok");
         email.classList.add("input-erro");
     }
 });
+
 /**== Senha ==*/
 
-function validarSenha(senha) {
+function tamanhosenha() {
 
-
-    return senha.length >= 8;
+    return senha.value.length >= 8;
 }
 
-senha.addEventListener("input",() =>{
-    const msg = document.getElementById("passwordmsg")
+/**== localStorage ==*/
 
-    if(senha.value.length >= 8) {
-        msg.textContent = "Senha válida."
-        msg.className = "ok"
-        msg.classList.add("input-ok")
+function cadastrarUsuario() {
+
+    const campos = [
+    nome,
+    cpf,
+    cep,
+    telefone,
+    email,
+    senha,
+    confirmarSenha
+];
+
+const existeCampoVazio = campos.some(
+    campo => campo.value.trim() === ""
+);
+
+if (existeCampoVazio) {
+    mostrarMensagem(
+        "Preencha todos os campos.",
+        "red"
+    );
+    return;
+}
+
+    if (!validarCPF(cpf.value)) {
+        mostrarMensagem("CPF inválido", "red");
+        return;
     }
 
-    else {
-        msg.textContent = "A senha deve ter no mínimo 6 caracteres."
-        msg.className = "erro"
-        msg.classList.add("input-erro")
+    if (!tamanhosenha()) {
+        mostrarMensagem ("A senha deve ter pelo menos 8 caracteres", "red");
+        return;
     }
-})
 
+    if (confirmarSenha.value !== senha.value) {
+        confirmarSenha.addEventListener("input", () => {
+            mostrarMensagem("As senhas não coincidem", "red");
+        })
+        return;
+    }
 
+    let usuarios =
+        JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    // CPF duplicado
+    const cpfExiste = usuarios.some(u => u.cpf === cpf.value);
+    if (cpfExiste) {
+        mostrarMensagem("CPF já cadastrado", "red");
+        return;
+    }
+
+    // EMAIL duplicado
+    const emailExiste = usuarios.some(u => u.email === email.value);
+    if (emailExiste) {
+        mostrarMensagem("E-mail já cadastrado", "red");
+        return;
+    }
+
+    const novoUsuario = {
+        nome: nome.value,
+        cpf: cpf.value,
+        cep: cep.value,
+        rua: rua.value,
+        bairro: bairro.value,
+        cidade: cidade.value,
+        estado: estado.value,
+        telefone: telefone.value,
+        email: email.value,
+        senha: senha.value
+    };
+
+    usuarios.push(novoUsuario);
+
+    localStorage.setItem( "usuarios", JSON.stringify(usuarios));
+
+    mostrarMensagem("Cadastro realizado com sucesso!");
+
+    setTimeout(() => {
+        window.location.href = "../index.html";
+    }, 2000);
+}
